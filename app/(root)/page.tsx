d0c8298c2f68ace4621aa57609ef64a106b73763
Durwankur Naik <ndurwankur14@gmail.com>
2023-09-14 11:36:23 +0530

    Added Clerk Auth and some UI components of shadcn.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  // This code was added because of the hydration error.
+  // Remember, if ever hydration error occurs in any project of next js, just use this technique\
+  // First finding out whether the component is mounted or not using states
+  // Then when its mounted, change the state to true so that the return won't be null anymore
+  
+  // const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect(() => {
+  //   setIsMounted(true);
+  // }, []);
+
+  // if (!isMounted) {
+  //   return null;
+  // }
+
+  return <div className="p-4">Root Page</div>;
+};
+
+export default SetupPage;
