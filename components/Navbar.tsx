a3f771e18d2b76c2c150629d620d9debdd880e9b
Durwankur Naik <ndurwankur14@gmail.com>
2023-09-17 16:42:22 +0530

    Done with the project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { UserButton, auth } from "@clerk/nextjs";
 
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
 import { MainNav } from "@/components/MainNav";
 import StoreSwitcher from "@/components/store-switcher";
-import { redirect } from "next/navigation";
-import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -24,6 +26,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
